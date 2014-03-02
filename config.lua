301ff8c3c0c4d0005e04d9690d57e83ca32a795d
Zyke Vince <vmarco.itsos@gmail.com>
2014-03-02 17:29:20 +0100

    Removed heavy vectors

--- config.lua
+++ config.lua
@@ -5,7 +5,7 @@ application =
         		width = 320,
                 height = 480,
                 scale = "Letterbox",
-                fps = 30,
+                fps = 60,
                 antialias = true,
                 audioPlayFrequency = 22050
                
